69d0ffa813a2a7471286539a96666742321a4d8f
sunjiaqi527 <41557471+sunjiaqi527@users.noreply.github.com>
2020-03-16 15:25:50 +0800

    Create menu.php

---
+++ content/menu.php
@@ -0,0 +1,23 @@
+<!-- sidenav class includes menu which divided into two parts: sidenav1&sidenav2-->
+		<div id="mySidenav" class="sidenav">
+			<div class="sidenav1" >
+				<!-- sidenav1 class includes the button which can close the menu. -->
+					<a href="javascript:void(0)" onclick="closeNav();closeNav1()" >&times;</a>
+			</div>
+			<div id="mySide1nav" class="sidenav2"> 			
+				<!-- sidenav2 class includes the content of the menu. -->
+					<a href="index.php" >HOME</a>
+					<a href="stepsmenu.php">STEPS</a>
+					<a href="about.php">ABOUT</a>
+			</div> 
+		</div>	
+
+
+<!-- topnav class is for the header of all pages which includes the button that can open the menu.-->			
+		<div id="topnav" class="topnav">
+				<a href="#" onclick="openNav();openNav1()"style="background-color: #E0E0E0;" ><img src="img/menu-icon.png" /></a>  
+		</div>
+<!-- topnavlogo class is next to the button and includes the logo. -->
+		<div class="topnavlogo">
+			<img src="img/horizontal-logo.png" />
+		</div>
