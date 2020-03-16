4a91c4571914cb1c77d165dcf1bbeffc606ccda1
sunjiaqi527 <41557471+sunjiaqi527@users.noreply.github.com>
2020-03-16 15:39:13 +0800

    Create step1.php

---
+++ steps/step1.php
@@ -0,0 +1,61 @@
+<div id="step1">
+	<h1> Step 1 </h1> 
+</div>
+			
+	<div id="video-container">
+		<div id="video">
+			<iframe width="100%" height="100%" src="https://www.youtube.com/embed/EJ4uVsSqQ9k" allowfullscreen="true" frameborder="0"> </iframe>
+		</div>
+	</div>
+			
+<div id="step1content">		
+			
+	<p>Once you have an idea and the motivation to start your own business, you need to answer the following questions.<p><br> 
+	<!-- change the list to checkbox later -->
+	<h2>What are your skills and interests?</h2>
+				
+				
+				
+	<h2>What are your weaknesses? </h2>
+	
+	
+						<p>Can you overcome them with help such as training and mentoring?</p>
+				<!--3--><p>Have you researched the market you are about to enter to find target customers and prospective competitors? 
+			                                            Find some help with your research <a href="http://www.business.vic.gov.au/marketing-and-sales/increasing-sales-through-marketing/doing-market-research">here</a></p> 
+				<!--4--><h2>Have you given a thought to hours of commitment, pricing of product or service? </h2>
+				
+				<!--5--><h2>Have you performed a SWOT analysis? </h2>
+							
+				
+                                                        SWOT analysis is a powerful tool to help you develop your business strategy. 
+														SWOT stands for Strengths, Weaknesses, Opportunities, and Threats. 
+														While your strengths and weaknesses are internal to your business, threats and opportunities are external in the environment. 
+														The main strategies behind SWOT analysis are: 
+														<ul>
+															<li>Strengths–Opportunities. Use your internal strengths to take advantage of opportunities. </li>
+															<li>Strengths-Threats. Use your strengths to minimize threats. </li>
+															<li>Weaknesses-Opportunities. Improve weaknesses by taking advantage of opportunities. </li>
+															<li>Weaknesses-Threats. Work to eliminate weaknesses to avoid threats.<br>
+															For more information click <a href="https://www.mindtools.com/pages/article/newTMC_05.htm ">here</a><br>
+															To watch a video explaining SWOT analysis click <a href="https://www.youtube.com/watch?time_continue=00&v=EJ4uVsSqQ9k">here</a><br>
+															To find and download SWOTanalysis templates click <a href="https://www.mindtools.com/pages/article/newTMC_05.htm?download=1 ">here</a>.</li>
+														</ul></br>
+				<!--6--><h3>A SWOT will help you analyze your strengths and weakness. </h3>
+	Manage your weaknesses by taking advantage of various courses offered at RNLC and NOX alliance and increase your success rate. </li>
+				
+	<ul>
+					<li><a href="https://rowvillenlc.org.au/index.php/en/business-development ">business courses</a></li>
+					<li><a href="https://rowvillenlc.org.au/index.php/en/workshops/marketing-success-detail">RNLC Social media marketing courses</a></li>
+					<li><a href="https://rowvillenlc.org.au/index.php/en/find-a-course/404/basic-computers-detail ">RNLC computer courses</a></li>
+					<li><a href="https://rowvillenlc.org.au/index.php/en/find-a-course/404/office-admin-detail ">RNLC Admin course</a></li>
+					<li>KNOX alliance courses</li>
+					<li>Knox council courses</li>
+					<li>To find extra help and guidance along the path of starting your own business, do visit RNLC & Knox council mentoring facilities links, here</a></li>
+					<li>To connect with likeminded people from the industry, do visit the RNLC & Knox council networking & Seminar events link, here</a></li>
+					<li><a href="http://www.business.vic.gov.au/">Vic Small Business website</a></li>
+	</ul>
+	</br>
+</div>			
+			
+
+	
